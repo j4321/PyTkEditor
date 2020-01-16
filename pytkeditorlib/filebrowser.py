cc5fe5e575428c4c2484780f5882085f7a889c7e
Juliette Monsel <j_4321@protonmail.com>
2020-01-16 17:06:31 +0100

    Add navigation history in file browser

--- pytkeditorlib/filebrowser.py
+++ pytkeditorlib/filebrowser.py
@@ -20,68 +20,15 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 GUI widget to browse local files
 """
-from tkinter import PhotoImage
-from tkinter.ttk import Treeview
+from tkinter.ttk import Treeview, Frame, Button
 from tkinter.font import Font
 import os
 
 from pytkeditorlib.autoscrollbar import AutoHideScrollbar as Scrollbar
-from pytkeditorlib.constants import IMAGES
 from pytkeditorlib.base_widget import BaseWidget
 
-
-class FileTree(Treeview):
-    def __init__(self, master, callback=print):
-        Treeview.__init__(self, master, show='tree', selectmode='none',
-                          style='flat.Treeview', padding=4)
-
-        self._im_file = PhotoImage(master=self, file=IMAGES['file'])
-        self._im_folder = PhotoImage(master=self, file=IMAGES['folder'])
-
-        self.font = Font(self, font="TkDefaultFont 9")
-        self.callback = callback
-
-        self.tag_configure('file', image=self._im_file)
-        self.tag_configure('folder', image=self._im_folder)
-        self.tag_bind('file', '<Double-1>', self._on_db_click_file)
-        self.tag_bind('folder', '<Double-1>', self._on_db_click_folder)
-        self.tag_bind('prev', '<Double-1>', self._on_db_click_prev)
-
-        self.bind('<1>', self._on_click)
-
-    def _on_click(self, event):
-        if 'indicator' not in self.identify_element(event.x, event.y):
-            self.selection_remove(*self.selection())
-            self.selection_set(self.identify_row(event.y))
-
-    def _on_db_click_file(self, event):
-        sel = self.selection()
-        if self.callback is not None and sel:
-            item = sel[0]
-            self.callback(item)
-
-    def _on_db_click_folder(self, event):
-        sel = self.selection()
-        if sel:
-            self.populate(sel[0])
-
-    def _on_db_click_prev(self, event):
-        sel = self.selection()
-        if sel:
-            self.populate(os.path.dirname(self.get_children()[1]))
-
-    def populate(self, path):
-        self.delete(*self.get_children())
-        p = os.path.abspath(path)
-        self.insert('', 0, '..', text='..', tags='prev')
-        self.insert('', 1, p, text=p, image=self._im_folder, open=True)
-        for (root, folders, files) in os.walk(p):
-            for f in sorted(folders):
-                self.insert(root, 'end', os.path.join(root, f), text=f,
-                            tags='folder')
-            for f in sorted(files):
-                self.insert(root, 'end', os.path.join(root, f), text=f,
-                            tags='file')
+# TODO: browsing history
+# TODO: filename filter
 
 
 class Filebrowser(BaseWidget):
@@ -90,21 +37,115 @@ class Filebrowser(BaseWidget):
         self.rowconfigure(1, weight=1)
         self.columnconfigure(0, weight=1)
 
-        self.filetree = FileTree(self, callback=callback)
+        self.history = []
+        self.history_index = -1
+
+        # --- browsing buttons
+        frame_btn = Frame(self)
+        self.b_up = Button(frame_btn, image='img_up', padding=0,
+                           command=self.browse_up)
+        self.b_backward = Button(frame_btn, image='img_left', padding=0,
+                                 command=self.browse_backward)
+        self.b_forward = Button(frame_btn, image='img_right', padding=0,
+                                command=self.browse_forward)
+        self.b_backward.pack(side='left', padx=2)
+        self.b_forward.pack(side='left', padx=2)
+        self.b_up.pack(side='left', padx=2)
+        self.b_forward.state(['disabled'])
+        self.b_backward.state(['disabled'])
+
+        # --- filetree
+        # self.filetree = FileTree(self, callback=callback)
+        self.filetree = Treeview(self, show='tree', selectmode='none',
+                                 style='flat.Treeview', padding=4)
         self._sx = Scrollbar(self, orient='horizontal', command=self.filetree.xview)
         self._sy = Scrollbar(self, orient='vertical', command=self.filetree.yview)
 
         self.filetree.configure(xscrollcommand=self._sx.set,
                                 yscrollcommand=self._sy.set)
 
+        self.font = Font(self, font="TkDefaultFont 9")
+        self.callback = callback
+
+        self.filetree.tag_configure('file', image='img_file')
+        self.filetree.tag_configure('folder', image='img_folder')
+        self.filetree.tag_bind('file', '<Double-1>', self._on_db_click_file)
+        self.filetree.tag_bind('folder', '<Double-1>', self._on_db_click_folder)
+
+        self.filetree.bind('<1>', self._on_click)
+
+        # --- placement
+        frame_btn.grid(row=0, column=0, sticky='ew', pady=2)
         self.filetree.grid(row=1, column=0, sticky='ewns')
         self._sx.grid(row=2, column=0, sticky='ew')
         self._sy.grid(row=1, column=1, sticky='ns')
 
+    def _on_click(self, event):
+        if 'indicator' not in self.filetree.identify_element(event.x, event.y):
+            self.filetree.selection_remove(*self.filetree.selection())
+            self.filetree.selection_set(self.filetree.identify_row(event.y))
+
+    def _on_db_click_file(self, event):
+        item = self.filetree.focus()
+        if self.callback is not None and item:
+            self.callback(item)
+
+    def _on_db_click_folder(self, event):
+        item = self.filetree.focus()
+        if item:
+            self.populate(item)
+
+    def history_add(self, path):
+        self.history_index += 1
+        self.history = self.history[:self.history_index]
+        self.history.append(path)
+        if self.history_index > 0:
+            self.b_backward.state(['!disabled'])
+        self.b_forward.state(['disabled'])
+        print(self.history_index, self.history)
+
+    def browse_up(self):
+        path = os.path.dirname(self.filetree.get_children()[0])
+        self.populate(path)
+
+    def browse_backward(self):
+        self.history_index -= 1
+        path = self.history[self.history_index]
+        self.populate(path, history=False)
+        self.b_forward.state(['!disabled'])
+        if self.history_index == 0:
+            self.b_backward.state(['disabled'])
+
+    def browse_forward(self):
+        self.history_index += 1
+        path = self.history[self.history_index]
+        self.populate(path, history=False)
+        self.b_backward.state(['!disabled'])
+        if self.history_index == len(self.history) - 1:
+            self.b_forward.state(['disabled'])
+
     def clear(self, event=None):
         self.filetree.delete(*self.filetree.get_children())
 
-    def populate(self, path):
+    def populate(self, path, history=True, reset=False):
+        self.configure(cursor='watch')
+        self.update_idletasks()
         self._sx.timer = self._sx.threshold + 1
         self._sy.timer = self._sy.threshold + 1
-        self.filetree.populate(path)
+
+        self.filetree.delete(*self.filetree.get_children())
+        p = os.path.abspath(path)
+        self.filetree.insert('', 1, p, text=p, image='img_folder', open=True)
+        for (root, folders, files) in os.walk(p):
+            for f in sorted(folders):
+                self.filetree.insert(root, 'end', os.path.join(root, f), text=f,
+                                     tags='folder')
+            for f in sorted(files):
+                self.filetree.insert(root, 'end', os.path.join(root, f), text=f,
+                                     tags='file')
+        self.configure(cursor='')
+        if reset:
+            self.history = []
+            self.history_index = -1
+        if history:
+            self.history_add(path)
