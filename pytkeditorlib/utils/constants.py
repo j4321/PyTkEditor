8e78c204c7d195f9a203a73974e72cc3173751c3
Juliette Monsel <j_4321@protonmail.com>
2020-05-12 22:21:09 +0200

    Fix parsing of Token.Comment.Cell with pygments Lexer

--- pytkeditorlib/utils/constants.py
+++ pytkeditorlib/utils/constants.py
@@ -45,7 +45,7 @@ REPORT_URL = f"https://gitlab.com/j_4321/{APP_NAME}/issues"
 
 class MyLexer(Python3Lexer):
     tokens = Python3Lexer.tokens.copy()
-    tokens['root'].insert(5, (r'^#( In\[.*\]| ?%%).*$', Comment.Cell))
+    tokens['root'].insert(4, (r'^#( In\[.*\]| ?%%).*$', Comment.Cell))
 
 
 PYTHON_LEX = MyLexer()
